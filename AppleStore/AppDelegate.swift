c078f14f217588dd40cac95ffb3a93af2d61c88c
dorofeevs <aleksandr.dorofeev@akvelon.com>
2022-10-06 17:49:17 +0400

    Initial commit

---
+++ AppleStore/AppDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.swift
+//  AppleStore
+//
+//  Created by Aleksandr Dorofeev on 06.10.2022.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    // Override point for customization after application launch.
+    return true
+  }
+}
---
+++ AppleStore/Controllers/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  AppleStore
+//
+//  Created by Aleksandr Dorofeev on 06.10.2022.
+//
+
+import UIKit
+
+/// Controller.
+class ViewController: UIViewController {
+
+  // MARK: - Life cycle.
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+}
